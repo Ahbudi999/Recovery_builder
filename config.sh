22fd46d29bfd5c9092a24c72290471b2a9d21548
Ahbudi999 <ahbudi999@gmail.com>
2024-05-12 01:53:54 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ export MANIFEST="https://github.com/ashyx/TWRP_Samsung_j2coreltedd"
 export BRANCH="twrp-9.0"
 
 
-export DEVICE="SM-J260G"
+export DEVICE="j2coreltedd"
 export DT_LINK="https://github.com/ashyx/TWRP_Samsung_j2coreltedd"
 export DT_BRANCH="twrp"
 export VENDOR="samsung"

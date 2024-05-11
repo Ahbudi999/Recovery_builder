928d641bebd7ab4a7d571c2fe58ea9ae22e13f46
Ahbudi999 <ahbudi999@gmail.com>
2024-05-11 23:59:17 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash env
 
-# about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+
+export NAME="twrp"
+export MANIFEST="https://github.com/ashyx/TWRP_Samsung_j2coreltedd"
+export BRANCH="twrp-9.0"
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export DEVICE="SM-J260G"
+export DT_LINK="https://github.com/ashyx/TWRP_Samsung_j2coreltedd"
+export DT_BRANCH="twrp"
+export VENDOR="samsung"
+export TARGET="recoveryimage"
+export TYPE="omni"
 
-# you dont need to edit these
+
 export SYM="_"

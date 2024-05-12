c795a8e268399e87b73bc24ea37c38f9dfb58a2e
Ahbudi999 <ahbudi999@gmail.com>
2024-05-12 12:09:21 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash env
 
 
-export NAME="twrp"
-export MANIFEST="https://github.com/ashyx/TWRP_Samsung_j2coreltedd"
-export BRANCH="twrp-9.0"
+export NAME= twrp
+export MANIFEST= https://github.com/ashyx/TWRP_Samsung_j2coreltedd
+export BRANCH= twrp-9.0
 
 
-export DEVICE="j2coreltedd"
-export DT_LINK="https://github.com/ashyx/TWRP_Samsung_j2coreltedd"
-export DT_BRANCH="twrp"
-export VENDOR="samsung"
-export TARGET="recoveryimage"
-export TYPE="omni"
+export DEVICE= j2coreltedd
+export DT_LINK= https://github.com/ashyx/TWRP_Samsung_j2coreltedd 
+export DT_BRANCH= twrp 
+export VENDOR= samsung 
+export TARGET= recoveryimage 
+export TYPE= omni 
 
 
-export SYM="_"
+export SYM= _
